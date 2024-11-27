e3ab1d861273968aef6a9bdb06883410c2e8bc8b
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-11-27 16:24:13 +0100

    additional test for US2

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -49,3 +49,13 @@ class TestIntelligentOffice(unittest.TestCase):
         system.manage_blinds_based_on_time()
         mock_servo.assert_called_with(2)
         self.assertFalse(system.blinds_open)
+
+    @patch.object(IntelligentOffice, "change_servo_angle")
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_should_not_open_blinds_if_sunday(self, mock_time_of_day: Mock, mock_servo: Mock):
+        d = datetime(2024, 12, 1, 8, 0)
+        mock_time_of_day.return_value = d
+        system = IntelligentOffice()
+        system.manage_blinds_based_on_time()
+        mock_servo.assert_called_with(2)
+        self.assertFalse(system.blinds_open)
