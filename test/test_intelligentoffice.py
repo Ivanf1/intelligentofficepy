20ed50c49fc693c890b6cae6e77176c09c29b2bd
Ivan Capobianco <capobianco.ivan@gmail.com>
2024-11-27 16:09:33 +0100

    red phase US2

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -19,3 +19,13 @@ class TestIntelligentOffice(unittest.TestCase):
     def test_check_occupancy_raises_error(self):
         system = IntelligentOffice()
         self.assertRaises(IntelligentOfficeError, system.check_quadrant_occupancy, -1)
+
+    @patch.object(IntelligentOffice, "change_servo_angle")
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_should_open_blinds(self, mock_time_of_day: Mock, mock_servo: Mock):
+        d = datetime(2024, 11, 27, 8, 0)
+        mock_time_of_day.return_value = d
+        system = IntelligentOffice()
+        system.manage_blinds_based_on_time()
+        mock_servo.assert_called_with(12)
+        self.assertTrue(system.blinds_open)
